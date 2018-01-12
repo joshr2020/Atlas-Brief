2452771dea12c0682b37d1a462afffac3ff6c355
stewy33 <slocumstewy@gmail.com>
2018-01-12 00:36:44 -0500

    tooling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import L from "leaflet";
 
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const mymap = L.map("mapid").setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic3Rld3kzMyIsImEiOiJjamNiN3ZkbTgwbnVrMnFtNjB0bHJ6bWdzIn0.aJrxPmXOJq0hX4BqJZJzVQ'
-}).addTo(mymap);
+    id: "mapbox.streets",
+    accessToken:
+      "pk.eyJ1Ijoic3Rld3kzMyIsImEiOiJjamNiN3ZkbTgwbnVrMnFtNjB0bHJ6bWdzIn0.aJrxPmXOJq0hX4BqJZJzVQ"
+  }
+).addTo(mymap);
