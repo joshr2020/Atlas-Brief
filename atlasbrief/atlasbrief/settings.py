ecdd0dfc4803724d4ac87e0ecc626733edb7ab68
Joshua Russo <jrusso@csail.mit.edu>
2018-01-16 18:26:12 -0500

    database set-up

--- atlasbrief/atlasbrief/settings.py
+++ atlasbrief/atlasbrief/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainsite.apps.MainsiteConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
