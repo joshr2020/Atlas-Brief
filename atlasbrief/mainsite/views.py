ab1deaad01f9f624f0e26b1ce2ed50549dedcadd
Joshua Russo <jrusso@csail.mit.edu>
2018-01-28 21:01:10 -0500

    briefs now returned with country profile request

--- atlasbrief/mainsite/views.py
+++ atlasbrief/mainsite/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse, JsonResponse
+
+from django.core import serializers
 
 from .models import Country
 
@@ -11,5 +13,6 @@ def index(request):
 def profile(request, country_name):
     country = get_object_or_404(Country, name=country_name)
     tag = country.tag
-    briefs = tag.brief_set.all()
-    return JsonResponse({'background': country.background, 'stats': country.stats})
+    #briefs = tag.brief_set.all()
+    briefs = serializers.serialize('json', tag.brief_set.all())
+    return JsonResponse({'background': country.background, 'stats': country.stats, 'briefs': briefs})
